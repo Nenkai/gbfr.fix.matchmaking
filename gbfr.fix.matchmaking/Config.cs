dc452cee03c0cc88da47b2671f452bc5e4ed94e2
Nenkai <Nenkai@users.noreply.github.com>
2024-04-03 17:56:33 +0200

    Add project files.

---
+++ gbfr.fix.matchmaking/Config.cs
@@ -0,0 +1,53 @@
+﻿using gbfr.fix.matchmaking.Template.Configuration;
+
+using Reloaded.Mod.Interfaces.Structs;
+
+using System.ComponentModel;
+
+namespace gbfr.fix.matchmaking.Configuration
+{
+    public class Config : Configurable<Config>
+    {
+        /*
+            User Properties:
+                - Please put all of your configurable properties here.
+    
+            By default, configuration saves as "Config.json" in mod user config folder.    
+            Need more config files/classes? See Configuration.cs
+    
+            Available Attributes:
+            - Category
+            - DisplayName
+            - Description
+            - DefaultValue
+
+            // Technically Supported but not Useful
+            - Browsable
+            - Localizable
+
+            The `DefaultValue` attribute is used as part of the `Reset` button in Reloaded-Launcher.
+        */
+
+        [DisplayName("Lobby Distance Filter")]
+        [Description("Controls AddRequestLobbyListDistanceFilter. Vanilla GBFR defaults to 'ELobbyDistanceFilterFar'.")]
+        [DefaultValue(ELobbyDistanceFilter.ELobbyDistanceFilterWorldwide)]
+        public ELobbyDistanceFilter LobbyDistanceFilter { get; set; } = ELobbyDistanceFilter.ELobbyDistanceFilterWorldwide;
+
+        public enum ELobbyDistanceFilter
+        {
+            ELobbyDistanceFilterClose,
+            ELobbyDistanceFilterDefault,
+            ELobbyDistanceFilterFar,
+            ELobbyDistanceFilterWorldwide,
+        }
+    }
+
+    /// <summary>
+    /// Allows you to override certain aspects of the configuration creation process (e.g. create multiple configurations).
+    /// Override elements in <see cref="ConfiguratorMixinBase"/> for finer control.
+    /// </summary>
+    public class ConfiguratorMixin : ConfiguratorMixinBase
+    {
+        // 
+    }
+}
