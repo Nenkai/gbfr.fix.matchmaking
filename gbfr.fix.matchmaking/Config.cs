5982e9a71083af8dfaba4bdcdd590589808d9428
Nenkai <Nenkai@users.noreply.github.com>
2024-04-25 21:32:53 +0200

    Autoupdate + num attempts slider

--- gbfr.fix.matchmaking/Config.cs
+++ gbfr.fix.matchmaking/Config.cs
@@ -33,6 +33,17 @@ namespace gbfr.fix.matchmaking.Configuration
         [DefaultValue(ELobbyDistanceFilter.ELobbyDistanceFilterWorldwide)]
         public ELobbyDistanceFilter LobbyDistanceFilter { get; set; } = ELobbyDistanceFilter.ELobbyDistanceFilterWorldwide;
 
+        [DisplayName("Join Lobby Attempts")]
+        [Description("Number of attempts until the game consider joining a lobby a fail. An attempt is made every 3 seconds. Vanilla is 7-10 attempts.")]
+        [DefaultValue(20)]
+        [SliderControlParams(minimum: 1.0, maximum: 100.0, smallChange: 1.0, largeChange: 10.0, tickFrequency: 10,
+            isSnapToTickEnabled: false,
+            tickPlacement: SliderControlTickPlacement.BottomRight,
+            showTextField: true,
+            isTextFieldEditable: true,
+            textValidationRegex: "\\d{1-3}")]
+        public int NumAttempts { get; set; } = 20;
+
         public enum ELobbyDistanceFilter
         {
             ELobbyDistanceFilterClose,
--- gbfr.fix.matchmaking/Mod.cs
+++ gbfr.fix.matchmaking/Mod.cs
@@ -13,98 +13,122 @@ using Reloaded.Memory.Interfaces;
 using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
 using IReloadedHooks = Reloaded.Hooks.ReloadedII.Interfaces.IReloadedHooks;
 using System;
+using Reloaded.Hooks.Definitions.Structs;
 
-namespace gbfr.fix.matchmaking
+namespace gbfr.fix.matchmaking;
+
+/// <summary>
+/// Your mod logic goes here.
+/// </summary>
+public unsafe class Mod : ModBase // <= Do not Remove.
 {
     /// <summary>
-    /// Your mod logic goes here.
+    /// Provides access to the mod loader API.
     /// </summary>
-    public class Mod : ModBase // <= Do not Remove.
+    private readonly IModLoader _modLoader;
+
+    /// <summary>
+    /// Provides access to the Reloaded.Hooks API.
+    /// </summary>
+    /// <remarks>This is null if you remove dependency on Reloaded.SharedLib.Hooks in your mod.</remarks>
+    private readonly IReloadedHooks? _hooks;
+
+    /// <summary>
+    /// Provides access to the Reloaded logger.
+    /// </summary>
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// Entry point into the mod, instance that created this class.
+    /// </summary>
+    private readonly IMod _owner;
+
+    /// <summary>
+    /// Provides access to this mod's configuration.
+    /// </summary>
+    private Config _configuration;
+
+    /// <summary>
+    /// The configuration of the currently executing mod.
+    /// </summary>
+    private readonly IModConfig _modConfig;
+
+    private static IStartupScanner? _startupScanner = null!;
+
+    private IHook<SetNumAttempts> _setNumAttemptsHook;
+    public delegate void SetNumAttempts(byte* a1);
+
+    public Mod(ModContext context)
     {
-        /// <summary>
-        /// Provides access to the mod loader API.
-        /// </summary>
-        private readonly IModLoader _modLoader;
+        _modLoader = context.ModLoader;
+        _hooks = context.Hooks;
+        _logger = context.Logger;
+        _owner = context.Owner;
+        _configuration = context.Configuration;
+        _modConfig = context.ModConfig;
 
-        /// <summary>
-        /// Provides access to the Reloaded.Hooks API.
-        /// </summary>
-        /// <remarks>This is null if you remove dependency on Reloaded.SharedLib.Hooks in your mod.</remarks>
-        private readonly IReloadedHooks? _hooks;
-
-        /// <summary>
-        /// Provides access to the Reloaded logger.
-        /// </summary>
-        private readonly ILogger _logger;
-
-        /// <summary>
-        /// Entry point into the mod, instance that created this class.
-        /// </summary>
-        private readonly IMod _owner;
-
-        /// <summary>
-        /// Provides access to this mod's configuration.
-        /// </summary>
-        private Config _configuration;
-
-        /// <summary>
-        /// The configuration of the currently executing mod.
-        /// </summary>
-        private readonly IModConfig _modConfig;
-
-        private static IStartupScanner? _startupScanner = null!;
-
-        public Mod(ModContext context)
+        var startupScannerController = _modLoader.GetController<IStartupScanner>();
+        if (startupScannerController == null || !startupScannerController.TryGetTarget(out _startupScanner))
         {
-            _modLoader = context.ModLoader;
-            _hooks = context.Hooks;
-            _logger = context.Logger;
-            _owner = context.Owner;
-            _configuration = context.Configuration;
-            _modConfig = context.ModConfig;
+            return;
+        }
 
-            var startupScannerController = _modLoader.GetController<IStartupScanner>();
-            if (startupScannerController == null || !startupScannerController.TryGetTarget(out _startupScanner))
+        var memory = Reloaded.Memory.Memory.Instance;
+
+        // Change func argument
+        SigScan("BA ?? ?? ?? ?? FF 50 ?? 48 8B 03", "", address =>
+        {
+            memory.SafeWrite((nuint)address + 1, new byte[] { (byte)_configuration.LobbyDistanceFilter });
+            _logger.WriteLine($"[gbfr.fix.matchmaking] Successfully set ELobbyDistanceFilter to {_configuration.LobbyDistanceFilter} ({(byte)_configuration.LobbyDistanceFilter}) at 0x{address:X8}", _logger.ColorGreen);
+        });
+
+        // Join lobby attempts are done 8-10 times, one attempt every 3 seconds. Once the counter reaches 0, joining a lobby fails
+        // The number of attempts is more or less randomized using std::random_device
+        //
+        // To more easily find this function, a function in it has a string "invalid random_device value"
+        // Find the function that matches:
+        //   *(_DWORD *)(a1 + 136) = (unsigned __int8)(BYTE4(randValue) + 20) / 3u;
+        SigScan("56 57 B8 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 29 C4 48 89 CE", "", address =>
+        {
+            _setNumAttemptsHook = _hooks.CreateHook<SetNumAttempts>(SetNumAttemptsHook, address).Activate();
+            _logger.WriteLine($"[gbfr.fix.matchmaking] Successfully hooked SetNumAttempts (num attempts: {_configuration.NumAttempts})", _logger.ColorGreen);
+        });
+    }
+
+    private static void SigScan(string pattern, string name, Action<nint> action)
+    {
+        var baseAddress = Process.GetCurrentProcess().MainModule!.BaseAddress;
+        _startupScanner?.AddMainModuleScan(pattern, result =>
+        {
+            if (!result.Found)
             {
                 return;
             }
-
-            SigScan("BA ?? ?? ?? ?? FF 50 ?? 48 8B 03", "", address =>
-            {
-                var memory = Reloaded.Memory.Memory.Instance;
-                memory.SafeWrite((nuint)address + 1, new byte[] { (byte)_configuration.LobbyDistanceFilter });
-
-                _logger.WriteLine($"[gbfr.fix.matchmaking] Successfully set ELobbyDistanceFilter to {_configuration.LobbyDistanceFilter} ({(byte)_configuration.LobbyDistanceFilter}) at 0x{address:X8}", _logger.ColorGreen);
-            });
-        }
-
-        private static void SigScan(string pattern, string name, Action<nint> action)
-        {
-            var baseAddress = Process.GetCurrentProcess().MainModule!.BaseAddress;
-            _startupScanner?.AddMainModuleScan(pattern, result =>
-            {
-                if (!result.Found)
-                {
-                    return;
-                }
-                action(result.Offset + baseAddress);
-            });
-        }
-
-        #region Standard Overrides
-        public override void ConfigurationUpdated(Config configuration)
-        {
-            // Apply settings from configuration.
-            // ... your code here.
-            _configuration = configuration;
-            _logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
-        }
-        #endregion
-
-        #region For Exports, Serialization etc.
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public Mod() { }
-#pragma warning restore CS8618
-        #endregion
+            action(result.Offset + baseAddress);
+        });
     }
+
+    private void SetNumAttemptsHook(byte* a1)
+    {
+        _setNumAttemptsHook.OriginalFunction(a1);
+        _logger.WriteLine($"[gbfr.fix.matchmaking] SetNumAttempts intercepted (original num attempts: {*(int*)(a1 + 0x88)}, new: {_configuration.NumAttempts})");
+
+        *(int*)(a1 + 0x88) = _configuration.NumAttempts;
+    }
+
+    #region Standard Overrides
+    public override void ConfigurationUpdated(Config configuration)
+    {
+        // Apply settings from configuration.
+        // ... your code here.
+        _configuration = configuration;
+        _logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
+    }
+    #endregion
+
+    #region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Mod() { }
+#pragma warning restore CS8618
+    #endregion
 }
